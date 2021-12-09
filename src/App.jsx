e06accb0e405d28b37c2cf05f6822de0528c3173
mohamed <devmohamedsayedibrahim@gmail.com>
2021-12-09 22:59:10 +0200

    signup ,logln ,contactus

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,18 @@ import { Route, Switch } from "react-router-dom";
 import { Nav } from "./components/Nav";
 import {Footer} from "./components/footer"
 import { ProductDetails } from "./Pages/product_details";
+// import { Signup } from "./Pages/Signup";
+// import { Login } from "./Pages/Login";
+// import { ContactUs } from "./Pages/ContactUs";
 export const App = () => {
   return (
     <>
       <Nav />
       <Photoslider />
       <Footer/>
+      {/* <Signup/>
+      <Login/>
+      <ContactUs/> */}
       
 
       <div className="container-fluid">
