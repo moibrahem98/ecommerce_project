4a0a29eee3ca2a54c13bb14e6071e6c6751da825
abdelrahman35 <abdelrahman.tarek.omar@gmail.com>
2021-12-13 19:51:27 +0200

    using redux

--- FinalProject/frontend/src/index.js
+++ FinalProject/frontend/src/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import './bootstrap.min.css'
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "./bootstrap.min.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./store";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
 
 reportWebVitals();
