bbcf45379f96c83c3bd405f820897dd77d6a04f4
moibrahem98 <mo.ibrahiim98@gmail.com>
2021-12-23 01:14:56 +0200

    Add Slider and edit scroll bar and some fixes

--- FinalProject/frontend/src/components/CarouselSlider.jsx
+++ FinalProject/frontend/src/components/CarouselSlider.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
-import slide1 from "../slide1.jpg";
-import slide2 from "../slide2.webp";
-import slide4 from "../slide4.jpg";
+import slide1 from "../images/1.jpg";
+import slide2 from "../images/2.jpg";
+import slide3 from "../images/3.jpg";
+import slide4 from "../images/4.jpg";
 
 export default function CarouselSlider() {
   return (
@@ -22,6 +23,13 @@ export default function CarouselSlider() {
         <img
           className="d-block w-100 carousel_img_main"
           src={slide2}
+          alt="Second slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100 carousel_img_main"
+          src={slide3}
           alt="Second slide"
         />
       </Carousel.Item>
--- FinalProject/frontend/src/pages/HomePage.jsx
+++ FinalProject/frontend/src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Container } from "react-bootstrap";
 import Product from "../components/Product";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
@@ -8,7 +8,8 @@ import { listProducts } from "../actions/productActions";
 import ProductCarousel from "../components/ProductCarousel";
 import CatSlider from "../components/CatSlider";
 // import ProductCarousel from "../components/ProductCarousel";
-// import CarouselSlider from "../components/CarouselSlider";
+import CarouselSlider from "../components/CarouselSlider";
+
 function HomeScreen({ history }) {
   const dispatch = useDispatch();
   const productList = useSelector((state) => state.productList);
@@ -21,31 +22,31 @@ function HomeScreen({ history }) {
   }, [dispatch, name]);
   return (
     <>
-      {/* <CarouselSlider /> */}
+      <CarouselSlider />
 
       {/* <CarouselPage /> */}
-      <div className="mt-5">
-        <h1 className="text-center" style={{ color: "#418993" }}>
-          Clean Beauty Favorites
-        </h1>
-        <CatSlider />
+      <Container>
+        <div className="mt-5">
+          <h3 className="heading_1">Clean Beauty Favorites</h3>
+          <CatSlider />
 
-        {!name && <ProductCarousel />}
-        <h1>Top Products</h1>
-        {loading ? (
-          <Loader></Loader>
-        ) : error ? (
-          <Message variant="danger">{error}</Message>
-        ) : (
-          <Row>
-            {products.map((product) => (
-              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                <Product product={product} />
-              </Col>
-            ))}
-          </Row>
-        )}
-      </div>
+          {!name && <ProductCarousel />}
+          <h3 className="heading_1 mt-5">Top Products</h3>
+          {loading ? (
+            <Loader></Loader>
+          ) : error ? (
+            <Message variant="danger">{error}</Message>
+          ) : (
+            <Row>
+              {products.map((product) => (
+                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                  <Product product={product} />
+                </Col>
+              ))}
+            </Row>
+          )}
+        </div>
+      </Container>
     </>
   );
 }
