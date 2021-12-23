8899f0edc5efe3417fb354860e7981db48b82db3
mohamed <devmohamedsayedibrahim@gmail.com>
2021-12-23 17:03:11 +0200

    edit update PaymentPage

--- FinalProject/frontend/src/pages/PaymentPage.jsx
+++ FinalProject/frontend/src/pages/PaymentPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Form, Button, Col } from "react-bootstrap";
+import { Form, Button, Col, Container, Row } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import FormContainer from "../components/FormContainer";
 import CheckoutSteps from "../components/CheckoutSteps";
@@ -40,53 +40,60 @@ function PaymentScreen({ history }) {
   };
 
   return (
-    <FormContainer>
+    <Container>
       <CheckoutSteps step1 step2 step3 />
-      <Form onSubmit={submitHandler}>
-        <Form.Group>
-          <Form.Label as="legend">Select Method</Form.Label>
-          <Col>
-            <Form.Check
-              type="radio"
-              label="Cash on Delievery"
-              id="cod" // cod is cash on delievery
-              name="paymentMethod"
-              value="Cash on Delievery"
-              onChange={(event) => {
-                setPaymentMethod(event.target.value);
-              }}
-            ></Form.Check>
-            <Form.Check
-              type="radio"
-              label="Paymob"
-              id="paymob"
-              value="paymob"
-              name="paymentMethod"
-              onChange={(event) => {
-                setPaymentMethod(event.target.value);
-              }}
-            ></Form.Check>
-          </Col>
-        </Form.Group>
-        {/* <input
-          type="button"
-          value="Paymob"
-          className="btn text-success btn_color"
-          onclick={window.open(
-            "http://127.0.0.1:8000/order/api/orders/payment/"
-          )}
-        /> */}
+      <Row className="mt-5">
+        <Col lg={5} md={6} sm={12} className="p-3 m-auto shadow rounded-lg">
+          <Form onSubmit={submitHandler}>
+            <Form.Group>
+              <h1 className=" text-succes mt-1 p-1 text-center ">
+                Select Payment Method
+              </h1>
+              <hr></hr>
+              <Col>
+                <Form.Check
+                  type="radio"
+                  label="Cash on Delievery"
+                  id="cod" // cod is cash on delievery
+                  name="paymentMethod"
+                  value="Cash on Delievery"
+                  onChange={(event) => {
+                    setPaymentMethod(event.target.value);
+                  }}
+                ></Form.Check>
+                <Form.Check
+                  type="radio"
+                  label="Paymob"
+                  id="paymob"
+                  value="paymob"
+                  name="paymentMethod"
+                  onChange={(event) => {
+                    setPaymentMethod(event.target.value);
+                  }}
+                ></Form.Check>
+              </Col>
+            </Form.Group>
+            {/* <input
+        type="button"
+        value="Paymob"
+        className="btn text-success btn_color"
+        onclick={window.open(
+          "http://127.0.0.1:8000/order/api/orders/payment/"
+        )}
+      /> */}
 
-        <br></br>
-        <Button
-          type="submit"
-          variant="dark"
-          className="w-100 text-success btn_color"
-        >
-          Continue
-        </Button>
-      </Form>
-    </FormContainer>
+            <br></br>
+            <Button
+              type="submit"
+              variant="dark"
+              className="w-100 text-success btn_color"
+            >
+              Continue
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
--- FinalProject/frontend/src/pages/UpdateProfilePage.jsx
+++ FinalProject/frontend/src/pages/UpdateProfilePage.jsx
@@ -55,62 +55,6 @@ function UpdateProfileScreen({ history }) {
     }
   };
   return (
-    // <Row>
-    //   <Col md={9}>
-    //     <h2>Update User Profile</h2>
-
-    //     {message && <Message variant="danger">{message}</Message>}
-    //     {error && <Message variant="danger">{error}</Message>}
-    //     {loading && <Loader />}
-    //     <Form onSubmit={submitHandler}>
-    //       <Form.Group controlId="name">
-    //         <Form.Label>Name</Form.Label>
-    //         <Form.Control
-    //           required
-    //           type="name"
-    //           placeholder="Enter name"
-    //           value={name}
-    //           onChange={(e) => setName(e.target.value)}
-    //         ></Form.Control>
-    //       </Form.Group>
-
-    //       <Form.Group controlId="email">
-    //         <Form.Label>Email Address</Form.Label>
-    //         <Form.Control
-    //           required
-    //           type="email"
-    //           placeholder="Enter Email"
-    //           value={email}
-    //           onChange={(e) => setEmail(e.target.value)}
-    //         ></Form.Control>
-    //       </Form.Group>
-
-    //       <Form.Group controlId="password">
-    //         <Form.Label>Password</Form.Label>
-    //         <Form.Control
-    //           type="password"
-    //           placeholder="Enter Password"
-    //           value={password}
-    //           onChange={(e) => setPassword(e.target.value)}
-    //         ></Form.Control>
-    //       </Form.Group>
-
-    //       <Form.Group controlId="passwordConfirm">
-    //         <Form.Label>Confirm Password</Form.Label>
-    //         <Form.Control
-    //           type="password"
-    //           placeholder="Confirm Password"
-    //           value={confirmPassword}
-    //           onChange={(e) => setConfirmPassword(e.target.value)}
-    //         ></Form.Control>
-    //       </Form.Group>
-
-    //       <Button type="submit" variant="dark" className="w-100 text-success btn_color">
-    //         Update
-    //       </Button>
-    //     </Form>
-    //   </Col>
-    // </Row>
     <Container>
     <Row className="mt-5">
     <Col lg={5} md={6} sm={12} className="p-3 m-auto shadow rounded-lg">
